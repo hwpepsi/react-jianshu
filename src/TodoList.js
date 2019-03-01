b36de68a9fa92f1f90b4b5a6a1d0c4782b896b88
hwpepsi <hwpepsi@126.com>
2019-03-01 11:14:28 +0800

    react-redux

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,69 +1,46 @@
-import React, { Component } from 'react';
-import "antd/dist/antd.css";
-import store from './store';
-import axios from 'axios';
-
-//import {CHANGE_INPUT_VALUE, ADD_TODO_ITEM, DELETE_TODO_ITEM} from './store/actionTypes'
-import {  getInitList ,getInputChangeAction, getAddItemAction, getDeleteItemAction, initListAction} from './store/actionCreator'
-import TodoListUI from './TodoListUi'
+import React ,{Component} from 'react'
+import store from './store'
+import {connect }from 'react-redux'
+import { dispatch } from 'rxjs/internal/observable/pairs';
 
 class TodoList extends Component {
   constructor(props){
     super(props);
-    this.state = store.getState();
-    //console.log(this.state);
-    this.handleInputChange = this.handleInputChange.bind(this);
-    this.handleStoreChange = this.handleStoreChange.bind(this);
-    this.handleButtonClick = this.handleButtonClick.bind(this);
-    this.handleItemDelete = this.handleItemDelete.bind(this);
-    store.subscribe(this.handleStoreChange)
+    //this.handleInputChange = this.handleInputChange.bind(this)
   }
-
   render() {
-    return (
-        <TodoListUI 
-          inputValue={this.state.inputValue}
-          list={this.state.list}
-          handleInputChange={this.handleInputChange}
-          handleButtonClick={this.handleButtonClick}
-          handleItemDelete={this.handleItemDelete}
-        />
-      )
+    return (<div>
+        <div>
+          <input value={this.props.inputValue} onChange={this.props.handleInputChange}/>
+          <button onClick={this.handle}>submit</button>
+        </div>
+        <ul>
+          <li>hello</li>
+        </ul>
+      </div>
+    )
   }
 
-  componentDidMount(){
-    const action =  getInitList();
-    store.dispatch(action);
-    console.log(action);
-    // const action = getTodoList();
-    // store.dispatch(action);
-    // axios.get(' https://www.easy-mock.com/mock/5c77437e8fb19e362fb034a5/api/list.json').then((res) => {
-    //     const data = res.data;
-    //     const action = initListAction(data);
-    //     store.dispatch(action);
-    // })
-  }
-
-  handleInputChange(e){
-    //console.log(e.target.value);
-    const action =  getInputChangeAction(e.target.value)
-    store.dispatch(action);
-  }
-
-  handleStoreChange(){
-    this.setState(store.getState());
-  }
-  
-  handleButtonClick(){
-    const action = getAddItemAction();
-    store.dispatch(action);
-  }
-
-  handleItemDelete(index){
-    const action = getDeleteItemAction(index);
-    store.dispatch(action);
-  }
 
 }
+//映射关系：state是store中的数据，映射到组件的props中
+const mapStateToProps = (state) =>{
+  return {
+    inputValue: state.inputValue
+  }
+}
 
-export default TodoList;
+//对store中的数据做修改，我们将store.dispatch挂载到props上，
+const mapDispatchToProps = (dispatch) =>{
+  return {
+    handleInputChange(e){
+      const action = {
+        type: 'change_input_value',
+        value: e.target.value
+      }
+      dispatch(action);
+    }
+  }
+}
+//connect make componenct connet with store
+export default connect(mapStateToProps, mapDispatchToProps)(TodoList) 
--- src/index.js
+++ src/index.js
@@ -1,6 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import TodoList from './TodoList';
+import {Provider} from 'react-redux'
+import store from './store'
+//provider连接store，让内部所有的组件能够获取store
+const App = (
+    <Provider store={store}>
+        <TodoList />
+    </Provider>
+)
 
-ReactDOM.render(<TodoList />, document.getElementById('root'));
+ReactDOM.render(App, document.getElementById('root'));
 
